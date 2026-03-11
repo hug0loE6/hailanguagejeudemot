dd056b1154eadb62cfb5565bb5c0ac53c33a18d1
hug0loE6 <hugo.lopez.20036@gmail.com>
2026-03-11 12:39:41 +0100

    ca recup toute les relations, par contre c'est que les relations id, faudrait plus d'info

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
 import requests
 # c'est :r{idrelation} pour avoir le node de la relation avec son nom relater venant de la relation
 # Récupérer l'input de l'utilisateur
-input = input("Entrez un input du format suivant : mot1 relation mot2\n")
 
-separer = input.split()
+
+def createmapping():
+    listee = {}
+    response = requests.get("https://jdm-api.demo.lirmm.fr/v0/relations_types")
+    data = response.json()
+    for type in data:
+        listee[type.get("name")] = type.get("id")
+    return listee
+
+allrelationsfound = []
+
+idParNom = createmapping()
+
+linput = input("Entrez un input du format suivant : mot1 relation mot2\n")
+
+separer = linput.split()
 if len(separer) != 3:
     print("Erreur : L'input doit être au format 'mot1 relation mot2'")
     exit()
 
 mot1, relation, mot2 = separer
 
-
 # URL de l'API
 url = f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{mot1}/to/{mot2}"
-
-
-
 
 # Effectuer une requête GET
 response = requests.get(url)
@@ -26,18 +36,19 @@ if response.status_code == 200:
     data = response.json()
     print(data)
     print("\n")
-
-
-
+    listerelations = data.get("relations")
 else:
     print(f"Erreur: l'un des deux mots n'existe pas.")
+    exit()
 
-
-if len(data.get("relations"))==0:
-    print("\n :( \n")
+if len(listerelations)==0:
+    print("\n Pas de relations directe \n")
+else:
+    for i in listerelations:
+        if i.get("type") == idParNom.get(relation):
+            allrelationsfound.append(i.get("id"))
 
 urltransi = f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{mot1}"
-    
 
 response = requests.get(urltransi)
 
@@ -57,9 +68,14 @@ if response.status_code == 200:
         
         response2 = requests.get(url)
         data=response2.json()
+        listerelations = data.get("relations")
 
-        if len(data.get("relations"))!=0:
-            print(i.get("name"))
-            
+        for r in listerelations:
+            if r.get("type") == idParNom.get(relation):
+                getrelation = requests.get(f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{mot1}/to/{i.get('name')}").json().get("relations")
+                for r2 in getrelation:
+                    allrelationsfound.append((r2.get("id"),r.get("id")))
+
+    print(allrelationsfound)
         
 
