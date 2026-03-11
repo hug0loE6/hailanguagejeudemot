156bf520bc559743682450de63f8442ffd353c6d
andy.grosu@etu.umontpellier.fr <andy.grosu@etu.umontpellier.fr>
2026-03-11 09:43:28 +0100

    azjfqkjlnfd

--- test.py
+++ test.py
@@ -14,6 +14,9 @@ mot1, relation, mot2 = separer
 # URL de l'API
 url = f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{mot1}/to/{mot2}"
 
+
+
+
 # Effectuer une requête GET
 response = requests.get(url)
 
@@ -22,5 +25,41 @@ if response.status_code == 200:
     # Récupérer les données JSON
     data = response.json()
     print(data)
+    print("\n")
+
+
+
 else:
     print(f"Erreur: l'un des deux mots n'existe pas.")
+
+
+if len(data.get("relations"))==0:
+    print("\n :( \n")
+
+urltransi = f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{mot1}"
+    
+
+response = requests.get(urltransi)
+
+# Vérifier que la requête a réussi
+if response.status_code == 200:
+    # Récupérer les données JSON
+    data2 = response.json()
+    print("\n")
+    listnode =data2.get("nodes")
+
+    for i in listnode[:]:
+        if i.get("type") == 200:
+            listnode.remove(i)
+
+    for i in listnode:
+        url= f"https://jdm-api.demo.lirmm.fr/v0/relations/from/{i.get('name')}/to/{mot2}"
+        
+        response2 = requests.get(url)
+        data=response2.json()
+
+        if len(data.get("relations"))!=0:
+            print(i.get("name"))
+            
+        
+
